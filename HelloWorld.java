02e97c94f01fff28c6c2c8239d9cc87aa8560d0f
shivam12-github <shivamgarg121197@gmail.com>
2026-06-28 11:19:27 +0530

    Changes back to normal

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("This is a basic Java program.(dropdown)");
     }
 }
